306e4910bba7bb49a2af64ca7a70ce0b570d3772
Eric D. Torres <etorresnotary@gmail.com>
2021-09-29 19:33:10 -0500

    promise to add project data to portfolio projects array

--- app.js
+++ app.js
@@ -75,7 +75,9 @@ const promptProject = (portfolioData) => {
       message: "Would you like to enter another project?",
       default: false,
     },
-  ]);
+  ]).then(projectData => {
+      portfolioData.projects.push(projectData)
+  })
 };
 
 promptUser()
