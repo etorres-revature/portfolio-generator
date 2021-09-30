dcc6e6f36193626257bf22d92467ca2671e02213
Eric D. Torres <etorresnotary@gmail.com>
2021-09-29 19:24:55 -0500

    promptProject

--- app.js
+++ app.js
@@ -11,15 +11,67 @@ const promptUser = () => {
       message: "What is your name?",
     },
     {
-      type: 'input',
-      name: 'github',
-      message: 'Enter your GitHub Username.'
+      type: "input",
+      name: "github",
+      message: "Enter your GitHub Username.",
     },
     {
-      type: 'input',
-      name: 'about',
-      message: 'Provide some information about yourself:'
-    }
+      type: "input",
+      name: "about",
+      message: "Provide some information about yourself:",
+    },
+  ]);
+};
+
+const promptProject = () => {
+  console.log(`
+    =================
+    Add a New Project
+    =================
+`);
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "projectName",
+      message: "What is the name of your project?",
+    },
+    {
+      type: "input",
+      name: "projectDescription",
+      message: "Provide a description of this project (REQUIRED).",
+    },
+    {
+      type: "checkbox",
+      name: "codingLanguage",
+      message: "What programming language did you use to build this project?",
+      choices: [
+        "JavaScript",
+        "HTML",
+        "CSS",
+        "ES6",
+        "jQuery",
+        "Bootstrap",
+        "Node",
+      ],
+    },
+    {
+      type: "input",
+      name: "projectLink",
+      message:
+        "Enter the link to the GitHub repository for this project (REQUIRED). ",
+    },
+    {
+      type: "confirm",
+      name: "feature",
+      message: "Would you like to feature this project repository?",
+      default: false,
+    },
+    {
+      type: "confirm",
+      name: "confirmAddProject",
+      message: "Would you like to enter another project?",
+      default: false,
+    },
   ]);
 };
 
