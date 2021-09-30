ab4aaf5ff21ca3a810f50308f868958745f8c6e9
Eric D. Torres <etorresnotary@gmail.com>
2021-09-29 19:46:51 -0500

    about when confirm

--- app.js
+++ app.js
@@ -32,15 +32,23 @@ const promptUser = () => {
       },
     },
     {
-        type: "confirm",
-        name: "confirmAbout",
-        message: "Would you like to enter information about yourself for an 'About' section? ",
-        default: true
+      type: "confirm",
+      name: "confirmAbout",
+      message:
+        "Would you like to enter information about yourself for an 'About' section? ",
+      default: true,
     },
     {
       type: "input",
       name: "about",
       message: "Provide some information about yourself:",
+      when: ({ confirmAbout }) => {
+        if (confirmAbout) {
+          return true;
+        } else {
+          return false;
+        }
+      },
     },
   ]);
 };
